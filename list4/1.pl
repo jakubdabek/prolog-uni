ef5b11010cc45f90165e333fd74a845ad8f86dc4
Jakub Dąbek <jakub.dabek@gmail.com>
2020-04-15 20:13:15 +0200

    Simplify expression finding

--- list4/1.pl
+++ list4/1.pl
@@ -1,42 +1,34 @@
-:- dynamic do_split_/3.
-
-% expression(Ns, Val, Expr) :- Expr is an expression consisting of
-% numbers from Ns in order with arithmetic operators inbetween,
-% whose value is Val
 expression_unordered(Ns, Val, Expr) :-
-    asserta((do_split_(A, B, C) :- !, split(A, B, C))),
-    expression_(Ns, Current, Expr),
-    Current is Val.
-expression_unordered(_, _, _) :- retractall(do_split_(_,_,_)), !, fail.
+    expression(unordered, Ns, Val, Expr).
 
 expression_ordered(Ns, Val, Expr) :-
-    asserta((do_split_(A, B, C) :- !, append(B, C, A))),
-    expression_(Ns, Current, Expr),
-    Current is Val.
-expression_ordered(_, _, _) :- retractall(do_split_(_,_,_)), !, fail.
+    expression(ordered, Ns, Val, Expr).
 
 nonempty([_|_]).
 
-expression_([], _, _, _) :- !, fail.
-expression_([Current], Current, Current) :- !.
-expression_(Ns, Current, Expr) :-
-    do_split_(Ns, Xs, Ys),
+split_order(ordered, XYs, Xs, Ys) :- !, append(Xs, Ys, XYs).
+split_order(unordered, XYs, Xs, Ys) :- !, split(XYs, Xs, Ys).
+
+% expression(+Order, +Ns, +Val, -Expr) :- Expr is an expression consisting of
+% numbers from Ns with arithmetic operators inbetween whose value is Val
+% Order (ordered/unordered) determines whether the elements from Ns appear in order in Expr
+expression(Order, Ns, Val, Expr) :-
+    expression(Order, Ns, Expr),
+    Val =:= Expr.
+
+expression(_, [], _) :- !, fail.
+expression(_, [Current], Current) :- !.
+expression(Order, Ns, Expr) :-
+    split_order(Order, Ns, Xs, Ys),
     nonempty(Xs), nonempty(Ys),
-    expression_(Xs, CurrentX, ExprX),
-    expression_(Ys, CurrentY, ExprY),
-    make_expr(CurrentX, CurrentY, ExprX, ExprY, Expr, Current).
+    expression(Order, Xs, ExprX),
+    expression(Order, Ys, ExprY),
+    make_expr(ExprX, ExprY, Expr).
 
-make_expr(Val1, Val2, Expr1, Expr2, Expr, Val) :-
-    arithmetic_op(Op),
-    \+ (Op = (/), Val2 =:= 0),
-    Expr =.. [Op, Expr1, Expr2],
-    Tmp =.. [Op, Val1, Val2],
-    Val is Tmp.
-
-arithmetic_op(+).
-arithmetic_op(-).
-arithmetic_op(*).
-arithmetic_op(/).
+make_expr(Expr1, Expr2, Expr1 + Expr2).
+make_expr(Expr1, Expr2, Expr1 - Expr2).
+make_expr(Expr1, Expr2, Expr1 * Expr2).
+make_expr(Expr1, Expr2, Expr1 / Expr2) :- Expr2 =\= 0.
 
 
 % split(XYs, Xs, Ys) :- XYs is split into 2 lists Xs and Ys
