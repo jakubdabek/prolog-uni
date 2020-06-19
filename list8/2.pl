7a2d2197431435c3f9a79e9604d4bff920afddcc
Jakub Dąbek <jakub.dabek@gmail.com>
2020-06-20 00:16:12 +0200

    Add Options to knapsack

--- list8/2.pl
+++ list8/2.pl
@@ -1,11 +1,12 @@
 :- use_module(library(clpfd)).
 
-% knapsack(+Values, +Weights, +Capacity, -Solution)
+% knapsack(+Values, +Weights, +Capacity, -Solution, ?Options)
 % finds the optimal solution to the 0-1 knapsack problem
-knapsack(Values, Weights, Capacity, Solution) :-
+knapsack(Values, Weights, Capacity, Solution, Options) :-
+    (var(Options) -> Options = []; true),
     same_length(Values, Weights),
     same_length(Values, Solution),
     Solution ins 0..1,
     scalar_product(Weights, Solution, #=<, Capacity),
     scalar_product(Values, Solution, #=, SolutionValue),
-    once(labeling([max(SolutionValue)], Solution)).
+    once(labeling([max(SolutionValue)|Options], Solution)).
