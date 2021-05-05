e735508c46c5d124f87522fa0bd635db8186b85e
Jakub Dąbek <jakub.dabek@gmail.com>
2021-05-05 16:15:46 +0200

    Update downloader for the other course

--- download-pdfs.sh
+++ download-pdfs.sh
@@ -6,3 +6,10 @@ done;
 mkdir -p lectures
 
 wget --no-clobber -P lectures/ "https://cs.pwr.edu.pl/kobylanski/dydaktyka/resources/pl_wyk"{1..14}".pdf"
+
+for i in {0..3};
+do
+    dir="constraint-programming-2021/list${i}";
+    mkdir -p "$dir";
+    wget --no-clobber -P "$dir" "https://cs.pwr.edu.pl/kobylanski/dydaktyka/resources/tw_lista_${i}.pdf";
+done
